365a685faab28cade6b73a3d69fabe8b778574cf
NoobScience <noobscience123@gmail.com>
2025-03-20 21:19:46 +0530

    feat: add main

---
+++ cmd/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log/slog"
+	"os"
+
+	"github.com/newtoallofthis123/easy_parse/api"
+	"github.com/newtoallofthis123/easy_parse/db"
+	"github.com/newtoallofthis123/easy_parse/utils"
+)
+
+func main() {
+	logger := slog.New(slog.NewJSONHandler(os.Stderr, nil))
+	logger.Info("Initialized Logger to STDERR")
+	env := utils.ReadEnv()
+	logger.Info("Initialized Environment Variables")
+
+	store, err := db.NewStore(env.ConnString)
+	if err != nil {
+		logger.Error(fmt.Sprintf("Failed to initialize database: %s", err.Error()))
+		return
+	}
+	logger.Info("Initialized Database")
+
+	api := api.NewApiServer(env.Port, logger, store)
+	err = api.Start()
+	if err != nil {
+		logger.Error(fmt.Sprintf("Failed to start API server: %s", err.Error()))
+		return
+	}
+}
